a9723e5a754c7f07d071558ad259e5489be09ab3
LiaOTCO <rosalia.otero@gmail.com>
2023-06-14 14:49:07 +0200

    actualizado menu responsive, funciones y modos

--- funcionesCalc.js
+++ funcionesCalc.js
@@ -12,15 +12,43 @@ function solve() {
   document.getElementById('result').value = y;
   return y;
 }
- 
 
-  const colorSwitch = document.querySelector('#switchmodo input[type="checkbox"]');
-  function cambiaTema(ev){
-      if(ev.target.checked){
-          document.documentElement.setAttribute('tema', 'dark');
-      } else {
-          document.documentElement.setAttribute('tema', 'light');
-      }
-  }
-  colorSwitch.addEventListener('change', cambiaTema);
 
+//modo oscuro
+const colorSwitch = document.querySelector('#switchmodo input[type="checkbox"]');
+const textSwitch = document.querySelector('#switchzoom input[type="checkbox"]');
+let oscuro = 0;
+function cambiaTema(ev) {
+    if (ev.target.checked) {
+        document.documentElement.setAttribute('tema', 'darknormal');
+        oscuro = 1;
+    } else {
+        document.documentElement.setAttribute('tema', 'lightnormal');
+        oscuro = 0;
+    }
+}
+//aumento texto
+function aumTexto(ev) {
+
+    if (ev.target.checked) {
+        document.documentElement.setAttribute('tema', 'darkgrande');
+        ocuro = 1;
+    }
+    else {
+        document.documentElement.setAttribute('tema', 'lightgrande');
+        oscuro = 0;
+    }
+}
+
+colorSwitch.addEventListener('change', cambiaTema);
+textSwitch.addEventListener('change', aumTexto);
+let imp = document.getElementsByClassName('claro');
+function modo() {
+    for (let i = 0; i < imp.length; i++) {
+        if (imp[i].className == 'claro' || imp[i].className == 'nada claro') {
+            imp[i].classList.add('oscuro');
+        } else {
+            imp[i].classList.remove('oscuro')
+        }
+    }
+}
